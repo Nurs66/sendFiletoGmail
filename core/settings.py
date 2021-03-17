a6cc9ee693e8eb8f3187b10b031fb7c0c47aa296
PythonNurs <nursultan.karagulov.dev@gmail>
2021-03-17 14:44:52 +0600

    change email

--- core/settings.py
+++ core/settings.py
@@ -142,8 +142,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'nursultandev@gmail.com'
-EMAIL_HOST_PASSWORD = 'zygnyqdgznqajrwk'
+EMAIL_HOST_USER = 'meerimsydygalieva93@gmail.com'
+EMAIL_HOST_PASSWORD = 'blnbyxojcwhlrvyj'
 
 
 django_heroku.settings(locals())
